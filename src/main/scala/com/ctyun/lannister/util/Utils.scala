2a667dd3da14a6ff975289bf54f61e1393a6fa9d
xutao <xutao_ustc@163.com>
2021-04-20 11:01:35 +0800

    Fix the problem that config file can not be read after assembly plugin added

--- src/main/scala/com/ctyun/lannister/util/Utils.scala
+++ src/main/scala/com/ctyun/lannister/util/Utils.scala
@@ -3,14 +3,16 @@ package com.ctyun.lannister.util
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.Constructor
 
+import java.io.FileInputStream
+
 object Utils {
   def getJvmUser:String = System.getProperty("user.name")
 
   def loadYmlDoc[T](path:String)(clazz:Class[T]):T={
-    val stream = getClass.getClassLoader.getResourceAsStream(path)
+    val url = getClass.getClassLoader.getResource(path)
+    val inputStream = new FileInputStream(url.getPath)
     val yaml = new Yaml(new Constructor(clazz))
-    yaml.load(stream).asInstanceOf[T]
+    yaml.load(inputStream).asInstanceOf[T]
   }
-
 }
 
