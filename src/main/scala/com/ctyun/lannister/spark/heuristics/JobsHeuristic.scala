eb139b6642068efb8d6f281962711711151929cc
xutao <xutao_ustc@163.com>
2021-04-29 00:36:29 +0800

    refactor

--- src/main/scala/com/ctyun/lannister/spark/heuristics/JobsHeuristic.scala
+++ src/main/scala/com/ctyun/lannister/spark/heuristics/JobsHeuristic.scala
@@ -7,113 +7,67 @@ import com.ctyun.lannister.spark.data.SparkApplicationData
 import org.apache.spark.JobExecutionStatus
 import org.apache.spark.status.api.v1.JobData
 
-/**
- * A heuristic based on metrics for a Spark app's jobs.
- *
- * This heuristic reports job failures and high task failure rates for each job.
- */
-class JobsHeuristic(private val heuristicConfigurationData: HeuristicConfigurationData)
-  extends Heuristic {
-  import JobsHeuristic._
+class JobsHeuristic(private val heuristicConfigurationData: HeuristicConfigurationData) extends Heuristic {
 
+  import JobsHeuristic._
 
   val jobFailureRateSeverityThresholds: SeverityThresholds =
     SeverityThresholds.parse(heuristicConfigurationData.getParams.get(JOB_FAILURE_RATE_SEVERITY_THRESHOLDS_KEY), ascending = true)
-
   val taskFailureRateSeverityThresholds: SeverityThresholds =
     SeverityThresholds.parse(heuristicConfigurationData.getParams.get(TASK_FAILURE_RATE_SEVERITY_THRESHOLDS_KEY), ascending = true)
 
-  override def getHeuristicConfData(): HeuristicConfigurationData = heuristicConfigurationData
+  override def getHeuristicConfData = ???
 
   override def apply(data: ApplicationData): HeuristicResult = {
     val evaluator = new Evaluator(this, data.asInstanceOf[SparkApplicationData])
-    def formatFailedJobs(failedJobs: Seq[JobData]): String = failedJobs.map(formatFailedJob).mkString("\n")
-
-    def formatFailedJob(jobData: JobData): String = f"job ${jobData.jobId}, ${jobData.name}"
-
-    def formatJobsWithHighTaskFailureRates(jobsWithHighTaskFailureRates: Seq[(JobData, Double)]): String =
-      jobsWithHighTaskFailureRates
-        .map { case (jobData, taskFailureRate) => formatJobWithHighTaskFailureRate(jobData, taskFailureRate) }
-        .mkString("\n")
-
-    def formatJobWithHighTaskFailureRate(jobData: JobData, taskFailureRate: Double): String =
-      f"job ${jobData.jobId}, ${jobData.name} (task failure rate: ${taskFailureRate}%1.3f)"
 
     val resultDetails = Seq(
       new HeuristicResultDetails("Spark completed jobs count", evaluator.numCompletedJobs.toString),
       new HeuristicResultDetails("Spark failed jobs count", evaluator.numFailedJobs.toString),
-      new HeuristicResultDetails("Spark failed jobs list", formatFailedJobs(evaluator.failedJobs)),
-      new HeuristicResultDetails("Spark job failure rate", f"${evaluator.jobFailureRate.getOrElse(0.0D)}%.3f"),
-      new HeuristicResultDetails(
-        "Spark jobs with high task failure rates",
-        formatJobsWithHighTaskFailureRates(evaluator.jobsWithHighTaskFailureRates)
+      new HeuristicResultDetails("Spark job failure rate", evaluator.jobFailureRate.getOrElse(0.0D).toString),
+      new HeuristicResultDetails("Spark failed jobs list", evaluator.failedJobs.map(job=>s"job ${job.jobId}, ${job.name}").mkString("\n")),
+      new HeuristicResultDetails("Spark jobs with high task failure rates",evaluator.jobsWithHighTaskFailureRates
+        .map { case (jobData, taskFailureRate) => s"job ${jobData.jobId}, ${jobData.name} (task failure rate: ${taskFailureRate})" }.mkString("\n")
       )
     )
-    val result = new HeuristicResult(
+
+    new HeuristicResult(
       heuristicConfigurationData.getClassname,
       heuristicConfigurationData.getName,
       evaluator.severity,
       0,
       resultDetails.toList
     )
-    result
   }
 }
 
 object JobsHeuristic {
-  /** The default severity thresholds for the rate of an application's jobs failing. */
-  val DEFAULT_JOB_FAILURE_RATE_SEVERITY_THRESHOLDS =
-    SeverityThresholds(low = 0.1D, moderate = 0.3D, severe = 0.5D, critical = 0.5D, ascending = true)
-
-  /** The default severity thresholds for the rate of a job's tasks failing. */
-  val DEFAULT_TASK_FAILURE_RATE_SEVERITY_THRESHOLDS =
-    SeverityThresholds(low = 0.1D, moderate = 0.3D, severe = 0.5D, critical = 0.5D, ascending = true)
-
   val JOB_FAILURE_RATE_SEVERITY_THRESHOLDS_KEY = "job_failure_rate_severity_thresholds"
-
   val TASK_FAILURE_RATE_SEVERITY_THRESHOLDS_KEY = "job_task_failure_rate_severity_thresholds"
 
   class Evaluator(jobsHeuristic: JobsHeuristic, data: SparkApplicationData) {
-    lazy val jobDatas: Seq[JobData] = data.store.store.jobsList(null)
+    lazy val jobDatas = data.store.store.jobsList(null)
+    lazy val failedJobs = jobDatas.filter { _.status == JobExecutionStatus.FAILED }
+    lazy val numCompletedJobs = jobDatas.count { _.status == JobExecutionStatus.SUCCEEDED }
+    lazy val numFailedJobs = jobDatas.count { _.status == JobExecutionStatus.FAILED }
+    lazy val jobFailureRate = { val numJobs = numCompletedJobs + numFailedJobs
+      if (numJobs == 0) None else Some(numFailedJobs.toDouble / numJobs.toDouble) }
+    private lazy val jobFailureRateSeverity = jobsHeuristic.jobFailureRateSeverityThresholds.severityOf(jobFailureRate.getOrElse[Double](0.0D))
 
-    lazy val numCompletedJobs: Int = jobDatas.count { _.status == JobExecutionStatus.SUCCEEDED }
+    private lazy val jobsAndTaskFailureRateSeverities = for {jobData <- jobDatas
+                                                          (taskFailureRate, severity) = taskFailureRateAndSeverityOf(jobData)
+                                                        } yield (jobData, taskFailureRate, severity)
+    lazy val jobsWithHighTaskFailureRates = jobsAndTaskFailureRateSeverities.filter { case (_, _, severity) => severity.id > Severity.MODERATE.id }
+                                                                            .map { case (jobData, taskFailureRate, _) => (jobData, taskFailureRate) }
 
-    lazy val numFailedJobs: Int = jobDatas.count { _.status == JobExecutionStatus.FAILED }
-
-    lazy val failedJobs: Seq[JobData] = jobDatas.filter { _.status == JobExecutionStatus.FAILED }
-
-    lazy val jobFailureRate: Option[Double] = {
-      // Currently, the calculation assumes there are no jobs with UNKNOWN or RUNNING state.
-      val numJobs = numCompletedJobs + numFailedJobs
-      if (numJobs == 0) None else Some(numFailedJobs.toDouble / numJobs.toDouble)
-    }
-
-    lazy val jobsWithHighTaskFailureRates: Seq[(JobData, Double)] =
-      jobsWithHighTaskFailureRateSeverities.map { case (jobData, taskFailureRate, _) => (jobData, taskFailureRate) }
-
+    private lazy val taskFailureRateSeverities: Seq[Severity] = jobsAndTaskFailureRateSeverities.map { case (_, _, severity) => severity }
     lazy val severity: Severity = Severity.max((jobFailureRateSeverity +: taskFailureRateSeverities): _*)
 
-    private lazy val jobFailureRateSeverityThresholds = jobsHeuristic.jobFailureRateSeverityThresholds
 
-    private lazy val taskFailureRateSeverityThresholds = jobsHeuristic.taskFailureRateSeverityThresholds
-
-    private lazy val jobFailureRateSeverity: Severity =
-      jobFailureRateSeverityThresholds.severityOf(jobFailureRate.getOrElse[Double](0.0D))
-
-    private lazy val jobsWithHighTaskFailureRateSeverities: Seq[(JobData, Double, Severity)] =
-      jobsAndTaskFailureRateSeverities.filter { case (_, _, severity) => severity.id > Severity.MODERATE.id }
-
-    private lazy val jobsAndTaskFailureRateSeverities: Seq[(JobData, Double, Severity)] = for {
-      jobData <- jobDatas
-      (taskFailureRate, severity) = taskFailureRateAndSeverityOf(jobData)
-    } yield (jobData, taskFailureRate, severity)
-
-    private lazy val taskFailureRateSeverities: Seq[Severity] =
-      jobsAndTaskFailureRateSeverities.map { case (_, _, severity) => severity }
 
     private def taskFailureRateAndSeverityOf(jobData: JobData): (Double, Severity) = {
       val taskFailureRate = taskFailureRateOf(jobData).getOrElse(0.0D)
-      (taskFailureRate, taskFailureRateSeverityThresholds.severityOf(taskFailureRate))
+      (taskFailureRate, jobsHeuristic.taskFailureRateSeverityThresholds.severityOf(taskFailureRate))
     }
 
     private def taskFailureRateOf(jobData: JobData): Option[Double] = {
