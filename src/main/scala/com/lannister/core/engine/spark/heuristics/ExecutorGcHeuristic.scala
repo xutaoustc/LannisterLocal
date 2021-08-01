79dd36ae89cf74457110e4c21e00612a586c9a47
xutao <xutao_ustc@163.com>
2021-08-01 23:47:28 +0800

    refactor

--- src/main/scala/com/lannister/core/engine/spark/heuristics/ExecutorGcHeuristic.scala
+++ src/main/scala/com/lannister/core/engine/spark/heuristics/ExecutorGcHeuristic.scala
@@ -4,34 +4,37 @@ import scala.collection.mutable.ListBuffer
 
 import com.lannister.core.conf.heuristic.HeuristicConfiguration
 import com.lannister.core.domain._
+import com.lannister.core.domain.{HeuristicResult => HR}
+import com.lannister.core.domain.{HeuristicResultDetail => HD}
 import com.lannister.core.engine.spark.fetchers.SparkApplicationData
+
 
 class ExecutorGcHeuristic(private val config: HeuristicConfiguration) extends Heuristic{
 
   import SeverityThresholds._
   val GC_SEVERITY_A_THRESHOLDS_KEY = "gc_severity_A_threshold"
   val GC_SEVERITY_D_THRESHOLDS_KEY = "gc_severity_D_threshold"
-  val gcSeverityAThresholds = parse(true, config.params.get(GC_SEVERITY_A_THRESHOLDS_KEY) )
-  val gcSeverityDThresholds = parse(false, config.params.get(GC_SEVERITY_D_THRESHOLDS_KEY) )
+  val thresholdsGcSeverityA = parse(config.params.get(GC_SEVERITY_A_THRESHOLDS_KEY))
+  val thresholdsGcSeverityD = parse(config.params.get(GC_SEVERITY_D_THRESHOLDS_KEY), false)
 
 
   override def apply(data: ApplicationData): HeuristicResult = {
     import ExecutorGcHeuristic._
     val evaluator = new Evaluator(this, data.asInstanceOf[SparkApplicationData])
 
-    var buf = ListBuffer(
-      HeuristicResultDetail("GC time to Executor Run time ratio", evaluator.ratio.toString),
-      HeuristicResultDetail("Total GC time", evaluator.gcTime.toString),
-      HeuristicResultDetail("Total Executor Runtime", evaluator.executorRunTimeTotal.toString)
+    var hds = ListBuffer(
+      HD("GC time to Executor Run time ratio", evaluator.ratio.toString),
+      HD("Total GC time", evaluator.gcTime.toString),
+      HD("Total Executor Runtime", evaluator.executorRunTimeTotal.toString)
     )
     if ( Severity.bigger(evaluator.severityTimeA, Severity.LOW) ) {
-      buf += HeuristicResultDetail("Gc ratio high", "The job is spending too much time on GC.")
+      hds += HD("Gc ratio high", "The job is spending too much time on GC.")
     }
     if ( Severity.bigger(evaluator.severityTimeD, Severity.LOW) ) {
-      buf += HeuristicResultDetail("Gc ratio low", "The job is spending too less time in GC.")
+      hds += HD("Gc ratio low", "The job is spending too less time in GC.")
     }
 
-    HeuristicResult(config.classname, config.name, evaluator.severityTimeA, 0, buf.toList)
+    HR(config.classname, config.name, evaluator.severityTimeA, 0, hds.toList)
   }
 }
 
@@ -43,7 +46,7 @@ object ExecutorGcHeuristic {
         .filterNot(_.id.equals("driver"))
         .foldLeft((0L, 0L))( (v, n) => (v._1 + n.totalGCTime, v._2 + n.totalDuration) )
     var ratio: Double = gcTime.toDouble / executorRunTimeTotal.toDouble
-    lazy val severityTimeA = executorGcHeuristic.gcSeverityAThresholds.of(ratio)
-    lazy val severityTimeD = executorGcHeuristic.gcSeverityDThresholds.of(ratio)
+    lazy val severityTimeA = executorGcHeuristic.thresholdsGcSeverityA.of(ratio)
+    lazy val severityTimeD = executorGcHeuristic.thresholdsGcSeverityD.of(ratio)
   }
 }
