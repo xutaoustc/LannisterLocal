af484378b8555ad840ba4b7058ee18ff86023c98
xutao <xutao_ustc@163.com>
2021-08-01 23:47:58 +0800

    refactor

--- src/main/scala/com/lannister/core/domain/SeverityThresholds.scala
+++ src/main/scala/com/lannister/core/domain/SeverityThresholds.scala
@@ -7,11 +7,12 @@ import com.lannister.core.domain.Severity.Severity
 
 case class SeverityThresholds(asc: JBool, low: JNum, moderate: JNum, severe: JNum, critical: JNum) {
   def of(value: JNum): Severity = Severity.getSeverity(value, asc, low, moderate, severe, critical)
+  def of(value: Long): Severity = of(value.asInstanceOf[JNum])
 }
 
 object SeverityThresholds {
-  def parse(asc: JBool, rawStr: String, tran: String => Double = _.toDouble): SeverityThresholds = {
-    val nums = rawStr.split(",").map(_.trim).map(tran)
+  def parse(s: String, asc: JBool = true, fm: String => Double = _.toDouble): SeverityThresholds = {
+    val nums = s.split(",").map(_.trim).map(fm)
     SeverityThresholds(asc, low = nums(0), moderate = nums(1), severe = nums(2), critical = nums(3))
   }
 }
