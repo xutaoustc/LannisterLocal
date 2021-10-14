9f0359a9146feebc426766c9b5f576e0c39dc804
xutao <xutao_ustc@163.com>
2021-10-14 14:34:07 +0800

    add "Total output bytes" and "Total output records"

--- src/main/scala/com/lannister/core/engine/spark/heuristics/TasksHeuristic.scala
+++ src/main/scala/com/lannister/core/engine/spark/heuristics/TasksHeuristic.scala
@@ -16,7 +16,9 @@ class TasksHeuristic (private val config: HeuristicConfiguration) extends Heuris
 
     val hds = Seq(
       HD("Spark completed tasks count", evaluator.totalTasksCount.toString),
-      HD("Spark result tasks count", evaluator.totalResultTasksCount.toString)
+      HD("Spark result tasks count", evaluator.totalResultTasksCount.toString),
+      HD("Total output bytes", evaluator.totalBytesWritten.toString),
+      HD("Total output records", evaluator.totalRecordsWritten.toString)
     )
 
     HR(config.classname, config.name, evaluator.severity, 0, hds.toList)
@@ -26,25 +28,31 @@ class TasksHeuristic (private val config: HeuristicConfiguration) extends Heuris
 object TasksHeuristic {
 
   class Evaluator(heuristic: TasksHeuristic, data: SparkApplicationData) {
-    private lazy val resultStageIDs = data.store.store.jobsList(null)
-      .flatMap { job =>
-        if (job.stageIds == None) { // not all job has stages
-          None
-        } else {
-          Option(job.stageIds.max)
-        }
-      }.toSet
-    private lazy val resultStages = data.store.store.stageList(null)
-      .filter(stage => resultStageIDs.contains(stage.stageId))
-    lazy val totalResultTasksCount = resultStages.map(stage => stage.numCompleteTasks).sum
+    private lazy val allJobs = data.store.store.jobsList(null)
+    private lazy val allStages = data.store.store.stageList(null)
 
-    private lazy val stageTasksData = data.store.store.stageList(null)
-      .flatMap(stage => stage.tasks)
-    lazy val totalTasksCount = stageTasksData.map(x => x.size).sum
+    private lazy val resultStageIDs = allJobs.flatMap {
+      job => if (job.stageIds.isEmpty) None else Option(job.stageIds.max)
+    }.toSet
+    private lazy val resultStages = allStages.filter { stg => resultStageIDs.contains(stg.stageId) }
+    lazy val totalResultTasksCount = resultStages.map { stg => stg.numCompleteTasks }.sum
+
+    private lazy val allTasks = allStages.flatMap { _.tasks }.flatMap(x => x)
+      .map { case (_, task) => task }
+    lazy val totalTasksCount = allTasks.size
+    lazy val (totalBytesWritten, totalRecordsWritten) = allTasks.foldLeft(0L, 0L) { (s, t) =>
+      val taskMetrics = t.taskMetrics
+      if (taskMetrics.isEmpty) {
+        s
+      } else {
+        ( s._1 + taskMetrics.get.outputMetrics.bytesWritten,
+          s._2 + taskMetrics.get.outputMetrics.recordsWritten)
+      }
+    }
+
 
 
     private lazy val tasksCountSeverity = heuristic.tasksCountSeverityThres.of(totalTasksCount)
-
     lazy val severity: Severity = Severity.max(tasksCountSeverity)
   }
 }
