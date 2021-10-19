997d97772d6fdedd5aaa66d2a07bdd8e3fd0971d
xutao <xutao_ustc@163.com>
2021-10-19 10:48:07 +0800

    add Spark failed hosts count and Spark failed error message count to AppHeuristic

--- src/main/scala/com/lannister/core/engine/spark/heuristics/AppHeuristic.scala
+++ src/main/scala/com/lannister/core/engine/spark/heuristics/AppHeuristic.scala
@@ -1,14 +1,18 @@
 package com.lannister.core.engine.spark.heuristics
 
+import scala.collection.mutable.ListBuffer
+
 import com.lannister.core.conf.HeuristicConfiguration
 import com.lannister.core.domain.{ApplicationData, Heuristic, HeuristicResult => HR, HeuristicResultDetail => HD, Severity}
 import com.lannister.core.domain.Severity.Severity
 import com.lannister.core.domain.SeverityThresholds.parse
 import com.lannister.core.engine.spark.fetchers.SparkApplicationData
 import com.lannister.core.engine.spark.heuristics.AppHeuristic.Evaluator
+import org.apache.commons.lang3.StringUtils
 
 import org.apache.spark.status.api.v1.TaskSorting._
 import org.apache.spark.status.api.v1.TaskStatus._
+
 
 class AppHeuristic (private val config: HeuristicConfiguration) extends Heuristic{
   val TASKS_COUNT_SEVERITY_THRES = "tasks_count_severity_thresholds"
@@ -17,17 +21,22 @@ class AppHeuristic (private val config: HeuristicConfiguration) extends Heuristi
   override def apply(data: ApplicationData): HR = {
     val evaluator = new Evaluator(this, data.asInstanceOf[SparkApplicationData])
 
-    val hds = Seq(
+    var hds = ListBuffer(
       HD("Spark completed tasks count", evaluator.totalCompleteTasksCount.toString),
       HD("Spark failed tasks count", evaluator.totalFailedTasksCount.toString),
       HD("Spark result tasks count", evaluator.totalResultTasksCount.toString),
       HD("Total input bytes", evaluator.inputBytesTotal.toString),
       HD("Total output bytes", evaluator.outputBytesTotal.toString),
       HD("Total shuffle read bytes", evaluator.shuffleReadBytesTotal.toString),
-      HD("Total shuffle write bytes", evaluator.shuffleWriteBytesTotal.toString),
-      HD("Spark failed hosts count", evaluator.hostsCount),
-      HD("Spark failed error message count", evaluator.errorMessagesCount)
+      HD("Total shuffle write bytes", evaluator.shuffleWriteBytesTotal.toString)
     )
+
+    if(StringUtils.isNotEmpty(evaluator.hostsCount)) {
+      hds += HD("Spark failed hosts count", evaluator.hostsCount)
+    }
+    if(StringUtils.isNotEmpty(evaluator.errorMessagesCount)) {
+      hds += HD("Spark failed error message count", evaluator.errorMessagesCount)
+    }
 
     HR(config.classname, config.name, evaluator.severity, 0, hds.toList)
   }
@@ -60,9 +69,9 @@ object AppHeuristic {
           stg.stageId, stg.attemptId, 0, Int.MaxValue, ID, (FAILED:: Nil).asJava
         ).map { task => (task.host, task.errorMessage) }
       }
-    val hostsCount = failedTasksHostAndMessage.map { case (h, _) => (h, 1) }
+    lazy val hostsCount = failedTasksHostAndMessage.map { case (h, _) => (h, 1) }
       .groupBy(_._1).map(t => (t._1, t._2.size)).map(_.productIterator.mkString(":")).mkString(",")
-    val errorMessagesCount = failedTasksHostAndMessage.map { case (_, m) => (m, 1) }
+    lazy val errorMessagesCount = failedTasksHostAndMessage.map { case (_, m) => (m, 1) }
       .groupBy(_._1).map(t => (t._1, t._2.size)).map(_.productIterator.mkString(":")).mkString(",")
 
 
